194f3a58862ce4a4822dfe6ede0f98f479d5e9d9
RUBENCERPAJIMENEZ <legacy.of.ruben@gmail.com>
2024-04-16 18:29:30 -0600

    Fist commit

---
+++ src/main/java/com/legacySoft/marveluniversesys/MarvelUniverseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.legacySoft.marveluniversesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelUniverseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelUniverseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legacySoft/marveluniversesys/MarvelUniverseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legacySoft.marveluniversesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelUniverseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
